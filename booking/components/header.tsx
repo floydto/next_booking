7ba4d272626ffedd77bdb52b25da3f7e77b29fd1
floydto <tosan21016@gmail.com>
2021-10-04 20:44:25 +0800

    123

--- booking/components/header.tsx
+++ booking/components/header.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import Image from 'next/image'
+import logo from '../public/logo.png'
 
 function Header() {
     return (
-        <header className="">
-            <h1>this is the header</h1>
-            <Image
-                className="object-contain"
-                src="https://i.stack.imgur.com/ZdC3w.png"
-                width={200}
-                height={100}
-            />
+        <header className="sticky top-0 z-50">
+            {/* Left */}
+            <div className="relative flex item-center h-10 cursor-pointer my-auto">
+                <Image
+                    className="object-contain"
+                    layout="fill"
+                    objectFit="contain"
+                    objectPosition="left"
+                    src={logo}
+                />
+            </div>
+            {/* Middle */}
+            <div></div>
+            {/* Right */}
+            <div></div>
         </header>
     )
 }
